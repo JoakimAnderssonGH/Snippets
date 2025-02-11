67c9c32b29773405899206f659f5c27843608ad3
Joakim Andersson <22893441+JoakimAnderssonGH@users.noreply.github.com>
2025-02-11 10:28:38 +0100

    Update Clock.py
    Refactored

--- Clock.py
+++ Clock.py
@@ -1,28 +1,33 @@
+""" 
+A clock application using Tkinter with localtime and 24hour format
+"""
 from time import strftime, localtime
 import tkinter as tk
 
 # Create a window
 root = tk.Tk()
 root.title('Digital Clock')
-
-# Set the background color to black and foreground color to white
-digital_clock = tk.Label(root, font=('calibri', 20, 'bold'), bg='black', fg='white')
-# Fill the entire window with ipadx and ipady for borders
+digital_clock = tk.Label(root, 
+                         font=('calibri', 20, 'bold'), 
+                         bg='black', 
+                         fg='white')
 digital_clock.pack(fill='both', expand=1, ipadx=50, ipady=10)
 
 # Function to update the time every second
 def update_time():
-    # Get the current time
-    string_time = strftime('%H:%M:%S', localtime())
-    # Update the time on the label
-    digital_clock.config(text=string_time)
-    # Call the function again after 1 second
+    # Set initial time
+    display_text = tk.StringVar()
+    initial_time = strftime('%H:%M:%S', localtime())
+    display_text.set(initial_time)
+
+    current_time = strftime('%H:%M:%S', localtime())
+    digital_clock.config(textvariable=display_text, text=current_time)
     digital_clock.after(1000, update_time)
 
+# Function to run the clock
 def clock():
-    # Call the function to update the time
     update_time()
     # Run the window
     root.mainloop()
-    
 
+#clock()
